4d9851ac0e6d00d316fc997a0dc1c7501cd8fd6d
LinkDima23 <dmytro.linkevych@kpk-lp.com.ua>
2020-11-16 15:38:47 +0200

    Add project files.

---
+++ PrKPZ4/ClassLibrary/Abit.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassLibrary
+{
+    public class ABit
+    {
+        private string subject;
+        private int bal;
+        public ABit(string subject = "", int bal = 0)
+        {
+            this.subject = subject;
+            this.bal = bal;
+        }
+        public void Init_Abit()
+        {
+            System.Console.WriteLine("Введiть предмет");
+ 
+            this.subject = System.Console.ReadLine();
+            
+            System.Console.WriteLine("Введiть бал");
+            
+            this.bal = Convert.ToInt32(System.Console.ReadLine());
+        }
+        ///////////////////////////////////////////////////
+        ///
+        
+        
+        
+        public ABit(ABit ABit)
+        
+        {
+        ////
+            this.subject = ABit.subject;
+            this.bal = ABit.bal;
+        }
+        public ABit(int bal)
+        {
+            this.subject = "";
+            this.bal = bal;
+        }
+        public void Set_subject(string subject)
+        {
+            this.subject = subject;
+        }
+        public void Set_bal(int bal)
+        {
+            this.bal = bal;
+        }
+        public string Get_subject()
+        {
+            return subject;
+        }
+        ////////////////////////////////////////////////////////
+        public int Get_bal() { return bal; }
+      
+        public void Show_Ed()
+        {
+            System.Console.WriteLine("Предмет - " + subject + " | бал за семестр - " + bal);
+        }
+    }
+}
---
+++ PrKPZ4/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using ClassLibrary;
+namespace PrKPZ4
+
+
+
+{
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            Teacher teacher = new Teacher();
+            Library_user library_User = new Library_user();
+            Applicant applicant = new Applicant();
+            Student student = new Student();
+            Human human = new Human();
+            Console.WriteLine("людина");
+            human.Init_Human();
+            human.Show_Info();
+            Console.WriteLine("студент");
+            student.Init_Student();
+            student.Show_Info();
+            Console.WriteLine("абітуріент");
+            applicant.Init_Aplicant();
+            applicant.Show_Info();
+            Console.WriteLine("вчитель");
+            teacher.Init_Teacher();
+            teacher.Show_Info();
+            Console.WriteLine("користувач");
+            library_User.Init_Library_user();
+            library_User.Show_Info();
+
+        }
+    }
+}
